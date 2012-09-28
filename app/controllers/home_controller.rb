09178b20b5512c02de9d0319a488e06b859ff91c
pjskennedy <peterjskennedy@gmail.com>
2012-09-27 22:24:49 -0400

    fixed token in vimeo oauth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
   def upload
     file_path = File.expand_path(params["file_loc"].as_json["tempfile"])
-    upload = Vimeo::Advanced::Upload.new("8b1856df9752bb3ff26f0a90a045834da678cff1", "946a7b18a5ca1c6decc03217ec6039e8557279b4", :token => "https://vimeo.com/oauth/request_token", :secret => "https://vimeo.com/oauth/authorize")
+    upload = Vimeo::Advanced::Upload.new("8b1856df9752bb3ff26f0a90a045834da678cff1", "946a7b18a5ca1c6decc03217ec6039e8557279b4", :token => "8697d319dcdb2e887c2677d148f4e2f3", :secret => "d916d3d2b1df25f7a72aa609159ba82492e36cc7")
     upload.upload(file_path)
     binding.pry
   end
